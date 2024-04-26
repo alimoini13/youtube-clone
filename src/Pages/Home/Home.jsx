ddbc705988089a141c6777ea5b19a8806fe4aa76
ali <moini_alessandro_13@yahoo.com>
2024-04-26 10:40:16 +0430

    feed component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import Feed from '../../Components/Feed/Feed';
 import Sidebar from '../../Components/Sidebar/Sidebar';
 import './Home.css';
 
@@ -5,6 +6,9 @@ const Home = ({ sidebar }) => {
   return (
     <>
       <Sidebar sidebar={sidebar} />
+      <div className={`container ${sidebar ? '' : 'large-container'}`}>
+        <Feed />
+      </div>
     </>
   );
 };
